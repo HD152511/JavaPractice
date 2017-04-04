48de058935f976ce0b889051a6a3e9f4a25539ba
alber <alber@192.168.198.1>
2017-04-04 12:05:18 +0900

    실습과제 입니다

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * <pre>
+ * org.dimigo.oop
+ *   |_Question
+ *
+ * 1. 개요 : 
+ * 2. 작성일: 2017. 3. 31.
+ *</pre>
+ *
+ *@author alber
+ */
+
+
+	/**
+	 * @param args
+	 */
+		public class Question {
+		    public static void main(String args[]){
+
+
+		        String question[] = {"가장 좋아하는 가수는? ", "가장 좋아하는 배우는? ","가장 좋아하는 과목은? "};
+		        String answer[] = {"Mobbdeep", "원빈","체육"};
+
+		        Scanner scan = new Scanner(System.in);
+
+		        for(int i=0;i < 3; i++){
+		            System.out.println((i+1) + "."+question[i]);
+		            System.out.println(scan.nextLine().equals(answer[i])?"정답입니다!":"틀렸습니다!");
+		        }
+
+		        StringBuilder sb = new StringBuilder();
+		        System.out.println(sb.append("<< 결과 출력 >>"));
+		        for(int i=0 ; i<3; i++){
+		            sb = new StringBuilder();
+		            System.out.println(sb.append(question[i] + answer[i] + "입니다").toString());
+		        }
+		    }
+		}
+		
+
