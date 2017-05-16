4e73d4c64ca1b9f99d22598e31851ece642c7af1
alber <alber@192.168.198.1>
2017-05-17 08:16:57 +0900

    실습과제 제출입니다. 어제 eclipse 오류로 인하여 다시 설치하고 코드를 짜느라 제출할 시간이 부족하여서 오늘 제출합니다.

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * 
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_Galaxy
+ *
+ * 1. 개요 : 
+ * 2. 작성일: 2017. 5. 12.
+ *</pre>
+ *
+ *@author alber
+ */
+public class Galaxy extends SmartPhone {
+	
+	public Galaxy(String model, String company, int price) {
+		super(model, company, price);
+		// TODO Auto-generated constructor stub
+	}
+	public void pay(){
+		
+	}
+	public void useWirelessCharging(){
+		
+	}
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/IPhone.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * 
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_IPhone
+ *
+ * 1. 개요 : 
+ * 2. 작성일: 2017. 5. 12.
+ *</pre>
+ *
+ *@author alber
+ */
+public class IPhone extends SmartPhone {
+
+	public IPhone(String model, String company, int price) {
+		super(model, company, price);
+		// TODO Auto-generated constructor stub
+	}
+	public void pay(){
+		
+	}
+	public void useAirDrop(){
+		
+	}
+
+}
