831cad12b48b283a18e5e578a9a6e5eb3ea8d40b
alber <alber@192.168.198.1>
2017-05-17 14:54:34 +0900

    .

--- JavaPractice/src/org/dimigo/inheritance/Galaxy.java
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -16,15 +16,18 @@ package org.dimigo.inheritance;
  *@author alber
  */
 public class Galaxy extends SmartPhone {
-	
+	public Galaxy() {
+						}
 	public Galaxy(String model, String company, int price) {
-		super(model, company, price);
-		// TODO Auto-generated constructor stub
-	}
-	public void pay(){
+		super(model,company,price);
+}
+	public void pay() {
+		System.out.println("삼성 페이로 결제합니다.");
 		
 	}
-	public void useWirelessCharging(){
+	public void useWirelessCharging() {
+		System.out.println("무선 충전 기능을 이용합니다.");
 		
 	}
+	
 }
--- JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -1,44 +1,36 @@
-/**
- * 
- */
 package org.dimigo.inheritance;
-
-/**
- * 
- * <pre>
- * org.dimigo.inheritance
- *   |_SmartPhone
- *
- * 1. 개요 : 
- * 2. 작성일: 2017. 5. 12.
- *</pre>
- *
- *@author alber
- */
 public class SmartPhone {
 	private String model;
 	private String company;
 	private int price;
-	
-	public SmartPhone(String model,String company,int price){
+	public SmartPhone() {
+		
+	}
+	public SmartPhone(String model, String company, int price) {
 		this.model=model;
 		this.company=company;
 		this.price=price;
 	}
-	public void turnOn(){
-		System.out.println("전원을 켭니다.");
+	public void turnOn() {
+		System.out.println(model+"의 전원을 켭니다.");
+		
 	}
-	public void turnOff(){
-		System.out.println("전원을 끕니다.");
+	public void turnOff() {
+		System.out.println(model+"의 전원을 끕니다.");
+		
 	}
-	public void pay(){
-		System.out.println("결제합니다.");
+	public void pay() {
+		
 	}
-	public void useSpecialFunction(SmartPhone sp){
-		System.out.println("기능을 사용합니다.");
+public void useSpecialFunction(SmartPhone phone) {
+	if(phone==null) return;
+	else if(phone instanceof IPhone) {
+		((IPhone) phone).useAirDrop();
 	}
-	public String toString(){
-		return "모델명 : "+model+","+"제조사 : "+company+","+"가격 : "+price +"원";
-	}
-
+	else if(phone instanceof Galaxy)
+		((Galaxy) phone).useWirelessCharging();
+}
+public String toString() {
+	return "모델명 : "+model+", 제조사 : "+company+", 가격 : "+String.format("%,d", price) +"원";
+}
 }
